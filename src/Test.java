c805292b02616ad203ca71d331365989bd07f311
jinhuizxc <1004260403@qq.com>
2018-04-14 11:43:28 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+/**
+ * 2018/4/14 全面学习整理java的知识
+ */
+
+public class Test {
+
+
+    public static void main (String[]args){
+        System.out.println("hwefwe");
+
+
+        String numString = "14";
+        System.out.println(Integer.parseInt(numString));
+//        String string = "15773003724";
+//        int phoneNum = Integer.parseInt(string);
+//        System.out.println(phoneNum);
+    }
+}
